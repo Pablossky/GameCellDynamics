18333d6a624e7906d8e37bace477e3cf4a16c29f
Pawel Mendzik <mendzik.pawel@gmail.com>
2023-12-01 15:47:33 +0100

    Powrot kroola (matryca zrobiona)

--- src/pages/hawkdovePage/HawkDovePage.tsx
+++ src/pages/hawkdovePage/HawkDovePage.tsx
@@ -1,101 +1,93 @@
 import React, { useState } from 'react';
 import './HawkDovePage.css';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-function HawkDoveModel() {
-    const [populationData, setPopulationData] = useState([{ round: 0, hawkFrequency: 0.5, doveFrequency: 0.5 }]);
-    const [v, setV] = useState(4); // Zysk z wygranej
-    const [c, setC] = useState(2); // Koszt eskalacji (poniesienia ran)
-
-    const calculatePayoffs = (hawkFrequency: number, doveFrequency: number) => {
-        const hawkPayoff = hawkFrequency * (v - c / 2);
-        const dovePayoff = doveFrequency * v;
-        return { hawkPayoff, dovePayoff };
-    };
-
-    const updateFrequencies = (currentRoundData: { hawkFrequency: number; doveFrequency: number }) => {
-        const { hawkFrequency, doveFrequency } = currentRoundData;
-
-        // Obliczamy payoffy dla jastrzębia i gołębia
-        const { hawkPayoff, dovePayoff } = calculatePayoffs(hawkFrequency, doveFrequency);
-
-        // Ustalamy progi wyboru strategii
-        const hawkThreshold = hawkPayoff / (hawkPayoff + dovePayoff);
-        const randomValue = Math.random();
-
-        // Aktualizujemy częstotliwości wystąpień na podstawie wylosowanej strategii
-        let newHawkFrequency, newDoveFrequency;
-
-        if (randomValue <= hawkThreshold) {
-            // Jastrząb kontra jastrząb - 50% szans na wygraną lub straty
-            newHawkFrequency = (hawkFrequency + 0.5) / 2;
-            newDoveFrequency = (doveFrequency + 0.5) / 2;
-        } else {
-            // Gołąb kontra gołąb - wygrana dzielona między nimi
-            newHawkFrequency = hawkFrequency / 2;
-            newDoveFrequency = doveFrequency / 2;
-        }
-
-        return { newHawkFrequency, newDoveFrequency };
-    };
-
-    const handleRunSimulation = () => {
-        const currentRoundData = populationData[populationData.length - 1];
-
-        // Aktualizujemy częstotliwości wystąpień na podstawie wyboru strategii
-        const { newHawkFrequency, newDoveFrequency } = updateFrequencies(currentRoundData);
-
-        const newRound = populationData.length;
-
-        setPopulationData([
-            ...populationData,
-            { round: newRound, hawkFrequency: newHawkFrequency, doveFrequency: newDoveFrequency }
-        ]);
-    };
-
-    return (
-        <div className="container">
-            <div className="input-container">
-                <h2>Badania Dynamiki Modelu Jastrzębia i Gołębia</h2>
-                <div>
-                    <label>Zysk z wygranej (v): </label>
-                    <input
-                        type="number"
-                        value={v}
-                        onChange={(e) => setV(Number(e.target.value))}
-                    />
-                </div>
-                <div>
-                    <label>Koszt eskalacji (c): </label>
-                    <input
-                        type="number"
-                        value={c}
-                        onChange={(e) => setC(Number(e.target.value))}
-                    />
-                </div>
-                <button onClick={handleRunSimulation}>Uruchom Symulację</button>
-            </div>
-            <div className="chart-container">
-                <ResponsiveContainer width="100%" height={400}>
-                    <LineChart data={populationData}>
-                        <XAxis dataKey="round" />
-                        <YAxis />
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <Tooltip />
-                        <Legend />
-                        <Line type="monotone" dataKey="hawkFrequency" name="Częstotliwość Jastrzębia" stroke="#8884d8" />
-                        <Line type="monotone" dataKey="doveFrequency" name="Częstotliwość Gołębia" stroke="#82ca9d" />
-                    </LineChart>
-                </ResponsiveContainer>
-            </div>
-        </div>
-    );
-}
+import {
+    ResponsiveContainer,
+    LineChart,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    Line,
+    Label,
+} from 'recharts';
 
 export const HawkDovePage = () => {
+
+    function HawkDoveModel() {
+        const [v, setV] = useState(4); // Zysk z wygranej
+        const [c, setC] = useState(2); // Koszt eskalacji (poniesienia ran)
+
+        const [round, setRound] = useState(0);
+        const [data, setData] = useState<{ round: number; hawkFrequency: number; doveFrequency: number }[]>([]);
+
+        const runSimulation = () => {
+            const newData = [...data];
+
+            const hawkPayoff = v - c / 2;
+            const dovePayoff = v;
+
+            const totalPayoff = hawkPayoff + dovePayoff;
+
+            const hawkPercentage = (hawkPayoff / totalPayoff) * 100;
+            const dovePercentage = (dovePayoff / totalPayoff) * 100;
+
+            newData.push({
+                round: round + 1,
+                hawkFrequency: hawkPercentage,
+                doveFrequency: dovePercentage,
+            });
+
+            setRound(round + 1);
+            setData(newData);
+        };
+
+        return (
+            <div className="container">
+                <div className="input-container">
+                    <h2>Badania Dynamiki Modelu Jastrzębia i Gołębia</h2>
+                    <div>
+                        <label>Zysk z wygranej (v): </label>
+                        <input
+                            type="number"
+                            value={v}
+                            onChange={(e) => setV(Number(e.target.value))}
+                        />
+                    </div>
+                    <div>
+                        <label>Koszt eskalacji (c): </label>
+                        <input
+                            type="number"
+                            value={c}
+                            onChange={(e) => setC(Number(e.target.value))}
+                        />
+                    </div>
+                    <button onClick={runSimulation}>Uruchom Symulację</button>
+                </div>
+                <div className="chart-container">
+                    <ResponsiveContainer width="100%" height={400}>
+                        <LineChart data={data}>
+                            <XAxis dataKey="round">
+                                <Label value="Runda" position="bottom" />
+                            </XAxis>
+                            <YAxis>
+                                <Label value="% Częstotliwość" angle={-90} position="insideLeft" />
+                            </YAxis>
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <Tooltip />
+                            <Legend />
+                            <Line type="monotone" dataKey="hawkFrequency" name="% Jastrzębia" stroke="#8884d8" />
+                            <Line type="monotone" dataKey="doveFrequency" name="% Gołębia" stroke="#82ca9d" />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div>
             <HawkDoveModel />
         </div>
     );
-};
+}
--- src/pages/hawkdovespacePage/HawkDoveSpacePage.tsx
+++ src/pages/hawkdovespacePage/HawkDoveSpacePage.tsx
@@ -1,45 +1,117 @@
-// Importowanie niezbędnych modułów
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect, useRef } from 'react';
+import * as d3 from 'd3';
 
-// Komponent reprezentujący kratownicę
-const Kratownica = styled.div`
-  width: 300px;
-  height: 300px;
-  border: 1px solid #000;
-  display: flex;
-  flex-wrap: wrap;
-`;
+interface Payoff {
+  Hawk: number;
+  Dove: number;
+}
 
-// Komponent reprezentujący pojedynczy fenotyp
-const Fenotyp = styled.div`
-  width: 20px;
-  height: 20px;
-  background-color: ${(props) => props.color};
-`;
+export const HawkDoveSpacePage: React.FC = () => {
+  const gridSize = 25;
+  const [rewardV, setRewardV] = useState<number>(4);
+  const [costC, setCostC] = useState<number>(2);
+  const [populationMatrix, setPopulationMatrix] = useState<number[][]>([]);
 
-// Główny komponent React
-function App() {
-  // Przykładowa kratownica fenotypów
-  const fenotypy = [
-    'red', 'green', 'blue', 'yellow', 'purple', 'orange',
-    'pink', 'brown', 'gray', 'cyan', 'magenta', 'lime'
-  ];
+  const cellSize = 20;
+  const padding = 2;
 
-  // Stan do przechowywania fenotypów
-  const [fenotypyState] = useState(fenotypy);
+  useEffect(() => {
+    const calculatePopulationMatrix = () => {
+      const matrix: number[][] = [];
+
+      for (let i = 0; i < gridSize; i++) {
+        const row: number[] = [];
+
+        for (let j = 0; j < gridSize; j++) {
+          const randomValue = Math.random(); // losowa liczba z zakresu [0, 1)
+
+          // Przydziel losowo wartość 1 (Hawk) lub 0 (Dove) na podstawie parametrów
+          const value = randomValue < getProbability(i, j) ? 1 : 0;
+
+          row.push(value);
+        }
+
+        matrix.push(row);
+      }
+
+      setPopulationMatrix(matrix);
+    };
+
+    const getProbability = (rowIndex: number, colIndex: number) => {
+      // Parametry warunkujące populację Hawk (1) lub Dove (0)
+      const baseProbability = 0.5;
+      const influenceRadius = 3; // Promień wpływu sąsiadujących pól
+  
+      let neighborsCount = 0;
+  
+      for (let i = Math.max(0, rowIndex - influenceRadius); i <= Math.min(gridSize - 1, rowIndex + influenceRadius); i++) {
+        for (let j = Math.max(0, colIndex - influenceRadius); j <= Math.min(gridSize - 1, colIndex + influenceRadius); j++) {
+          if (populationMatrix[i] && populationMatrix[i][j]) {
+            neighborsCount += populationMatrix[i][j];
+          }
+        }
+      }
+  
+      const neighborsRatio = neighborsCount / (influenceRadius * influenceRadius * 4); // Normalizacja do [0, 1]
+  
+      // Wpływ sąsiadów na prawdopodobieństwo
+      const probability = baseProbability + (neighborsRatio - baseProbability) * 0.5;
+  
+      return probability;
+    };
+
+    calculatePopulationMatrix();
+  }, [rewardV, costC]);
+
+  const chartRef = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    if (populationMatrix.length > 0) {
+      drawChart();
+    }
+  }, [populationMatrix]);
+
+  const drawChart = () => {
+    const svg = d3.select(chartRef.current);
+
+    // Clear previous chart
+    svg.selectAll('*').remove();
+
+    // Create the matrix
+    const rows = svg.selectAll('.row').data(populationMatrix).enter().append('g').attr('class', 'row');
+
+    const cells = rows
+      .selectAll('.cell')
+      .data((d: number[], i) => d.map((cell) => ({ value: cell, rowIndex: i })))
+      .enter()
+      .append('rect')
+      .attr('class', 'cell')
+      .attr('x', (d, i) => i * (cellSize + padding))
+      .attr('y', (d) => d.rowIndex * (cellSize + padding))
+      .attr('width', cellSize)
+      .attr('height', cellSize)
+      .style('fill', (d) => (d.value === 1 ? 'steelblue' : 'lightgreen'));
+  };
 
   return (
     <div>
-      <h1>Analiza modelu "jastrząb-gołąb"</h1>
-      <p>Następnym etapem analizy modelu jest wprowadzenie mieszanych gier przestrzennych. W MSEG również wykorzystamy losowo wygenerowaną kratownicę początkową.</p>
-      <Kratownica>
-        {fenotypyState.map((kolor, index) => (
-          <Fenotyp key={index} color={kolor} />
-        ))}
-      </Kratownica>
+      <h2>HawkDoveSpace Population Matrix</h2>
+      <p>Enter Reward (V) and Cost (C) values:</p>
+
+      <div>
+        <label>Reward (V): </label>
+        <input type="number" value={rewardV} onChange={(e) => setRewardV(Number(e.target.value))} />
+      </div>
+
+      <div>
+        <label>Cost (C): </label>
+        <input type="number" value={costC} onChange={(e) => setCostC(Number(e.target.value))} />
+      </div>
+
+      <h3>Population Matrix:</h3>
+      <svg ref={chartRef} width={gridSize * (cellSize + padding)} height={gridSize * (cellSize + padding)} />
     </div>
   );
-}
+};
 
-export default App;
+export default HawkDoveSpacePage;
